6ff0978a755a6d9663cc5a6a7fefadfcd7ddca7c
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2022-12-30 17:22:20 +0700

    add: search context

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.html', {'name': 'CodeWizz'})
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm': searchTerm})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
