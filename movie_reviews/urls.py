71c76ef1e3c683a8abeab2ccc843f9c2430d3a94
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2022-12-31 09:20:48 +0700

    update: serving the store image

--- movie_reviews/urls.py
+++ movie_reviews/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from movie import views as movie_views
 
 urlpatterns = [
@@ -8,3 +10,5 @@ urlpatterns = [
     path('about/', movie_views.about, name="about"),
     path('signup/', movie_views.signup, name="signup"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
