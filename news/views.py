f4f1ba5a149f7317c87d5a0aadcb5f69bc18dced
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2023-01-04 09:08:05 +0700

    add: news template and style to content

--- news/views.py
+++ news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import News
 
 
 def news(request):
-    return render(request, 'news.html')
+    news_list = News.objects.all().order_by('-date')
+    return render(request, 'news.html', {'news_list': news_list})
